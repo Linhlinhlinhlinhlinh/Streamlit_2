ecb38c6ba87ce1b6989e265c9f36453dd36e5e49
Lynn <145916700+Linhlinhlinhlinhlinh@users.noreply.github.com>
2024-03-19 20:50:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import time
-st.button(label= 'login')
-p = st.progress(0)
-for i in range(100):
-  p.progress(i+1)
-  time.sleep(0.1)
-st.balloons()
+st.title("Điền thông tin giới thiệu bản thân em")
+st.text_input('Họ và tên')
+st.text_input('Ngày tháng năm sinh')
+st.text_input('Giới tính')
+s = st.button('Submit')
+if s == True:
+  p = st.progress(0)
+  for i in range(100):
+    p.progress(i+1)
+    time.sleep(0.1)
+  st.balloons()
