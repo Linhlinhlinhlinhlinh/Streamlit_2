d5ca76952548f1b0a95f4645f94214eefddd947b
Lynn <145916700+Linhlinhlinhlinhlinh@users.noreply.github.com>
2024-03-24 20:33:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import time
 st.title("Điền thông tin giới thiệu bản thân em")
-st.text_input('Họ và tên')
-st.text_input('Ngày tháng năm sinh')
-st.text_input('Giới tính')
+name = st.text_input('Họ và tên')
+birthday = st.text_input('Ngày tháng năm sinh')
+gender = st.text_input('Giới tính')
 s = st.button('Submit')
 if s == True:
-  p = st.progress(0)
-  for i in range(100):
-    p.progress(i+1)
-    time.sleep(0.1)
-  st.snow()
-  st.empty()
+  if name != '' and gender != "" and birthday != '':
+    p = st.progress(0)
+    for i in range(100):
+      p.progress(i+1)
+      time.sleep(0.1)
+    st.snow()
+    st.empty()
+    st.title('Chào mừng em đến với bảng thống kê điểm!')
+  else: 
+    st.warning('Vui lòng nhập đầy đủ thông tin')
